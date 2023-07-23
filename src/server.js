694371e00f0997c765ba70c259f8e129dabce363
hendrix <leandrotully@gmail.com>
2023-07-23 10:17:44 -0300

    Ajuste import do pacote socket.oi

--- src/server.js
+++ src/server.js
@@ -1,20 +1,32 @@
+console.clear()
 import express from "express"
 import url from "url"
 import path from "path"
+import { Server } from "socket.io"
+import http from 'http'
+const io = new Server(http)
 
-console.clear()
 const app = express()
 const port = process.env.PORT || 3000
 
 //Obtém o caminho para o arquivo atual, nesse caso server.js
 const actualPath = url.fileURLToPath(import.meta.url)
 
-// Obtem o caminho para o diretório publico
+// A partir do diretório atual, podemos obter o caminho para o diretório publico
 const publicPath = path.join(actualPath, "../../", "public")
 
 // Acesso ao index do html
 app.use(express.static(publicPath))
 
-app.listen(PORT, () => {
-    console.log("Server is running at 3000")
+const httpServer = http.createServer(app)
+
+httpServer.listen(port, () => {
+    console.log(`Server is running at ${port}`)
 })
+
+io.on('connection', () => console.log("oi"))
+
+
+
+
+
