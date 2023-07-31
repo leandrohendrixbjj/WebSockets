2a246817c0c3d28ecd362d5ac52c92abaf15b4b5
hendrix <leandrotully@gmail.com>
2023-07-31 08:26:21 -0300

    Emitindo eventos

--- public/js/documento.js
+++ public/js/documento.js
@@ -1,1 +1,7 @@
 const socket = io();
+
+const textEditor = document.getElementById('editor-texto')
+
+textEditor.addEventListener('keyup', (event) => {
+    socket.emit('textEditor', textEditor.value)
+})
--- src/socket-back.js
+++ src/socket-back.js
@@ -3,4 +3,10 @@ import io from "./server.js";
 // Captura se alguém se conectou a pagina html
 io.on("connection", (socket) => {
     console.log(`A user is connected ${socket.id}`);
+
+    // Captura se alguém digitou um texto
+    socket.on('textEditor', (text) => {
+        console.log('Text', text)
+    })
 });
+
